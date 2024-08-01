dc46fe44f3435ae1ed233cf7486db6a1079c9b66
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-01 11:21:31 -0300

    add user routes

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import * as userController from '../controllers/user.controller.js'
+
+const userRoutes = Router()
+
+userRoutes.post('/register', userController.POSTRegisterUser)
+.post('/login', userController.POSTLoginUser)
