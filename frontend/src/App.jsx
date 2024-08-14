0fdfbe5552068044145d6066ff46ef2fd842fedc
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-14 13:16:22 -0300

    add isLoading modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { useEffect } from 'react';
 import Home from './screens/Home'
 import Auth from './screens/Auth'
 import Header from './components/Header/Header.jsx';
+import Modal from './components/Modal/Modal.jsx';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner.jsx';
 
 function App() {
 
-  const { auth } = usePatientContext()
-  
+  const { auth, isLoading } = usePatientContext()
+
   useEffect(() => {
     wakeUpServer()
   }, [])
@@ -17,6 +19,9 @@ function App() {
     <>
       <Header />
       {auth ? <Home /> : <Auth />}
+      <Modal isOpen={isLoading}>
+        <LoadingSpinner light={true} />
+      </Modal>
     </>
   )
 }
