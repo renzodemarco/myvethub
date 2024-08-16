fa0970340f5564acf046524264598bf4c841f43b
Renzo Demarco <renzo11demarco@gmail.com>
2024-08-16 15:07:52 -0300

    change jwt expiresIn

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -3,6 +3,6 @@ import '../config/env.config.js'
 
 const SECRET = process.env.JWT_SECRET
 
-export const generateToken = (payload) => jwt.sign(payload, SECRET, { expiresIn: '240h' })
+export const generateToken = (payload) => jwt.sign(payload, SECRET, { expiresIn: '2mo' })
 
 export const verifyToken = (token) => jwt.verify(token, SECRET)
